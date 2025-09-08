e407d706e36c625b87a81fdcaa9982372f454acf
Sharnisha J <jsharnishaxlb@gmail.com>
2025-09-08 19:12:09 +0530

    Add files via upload

---
+++ MavenProjectBasicsSTS/src/test/java/com/example/demo/MavenProjectBasicsStsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MavenProjectBasicsStsApplicationTests {
+
+	@Test
+    public void testApp() {
+        assertTrue(true);  // Dummy test
+    }
+
+}
